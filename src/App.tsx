8d548d5ff11f07476f06d6cd4ee24cccd6d64a6c
Lee Yuan Hooi <ho0i97@hotmail.com>
2021-10-05 10:53:37 +0800

    make font size bigger and give border black and gray color

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import React, { useRef } from "react";
-import Sudoku from "./components/Sudoku";
-import { ques } from "./components/ques";
-import { ans } from "./components/ans";
+import React, { useRef } from 'react';
+import Sudoku from './components/Sudoku';
+import ques from './components/ques';
+import ans from './components/ans';
 
-import "./App.css";
-import ReactToPrint from "react-to-print";
+import './App.css';
+import ReactToPrint from 'react-to-print';
 
 function App() {
   const inputRef = useRef<any>(null);
 
   return (
-    <div className="App">
-      <ReactToPrint trigger={() => <button>Print this out!</button>} content={() => inputRef.current} />
+    <div className='App'>
+      <ReactToPrint
+        trigger={() => <button>Print this out!</button>}
+        content={() => inputRef.current}
+      />
 
-      <div className="sudoku-collection" ref={inputRef}>
+      <div className='sudoku-collection' ref={inputRef}>
         {ans.map((datum: Array<Array<number | string>>, index: number) => {
           return <Sudoku index={index + 1} data={datum} key={index} />;
           //
--- src/components/Sudoku.tsx
+++ src/components/Sudoku.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import "./Sudoku.css";
-import CSS from "csstype";
+import React from 'react';
+import './Sudoku.css';
+import CSS from 'csstype';
 
 type Props = {
   index: number;
@@ -9,7 +9,7 @@ type Props = {
 const Sudoku: React.FC<Props> = ({ index, data }) => {
   const TOP_LEFT = [0, 3, 6];
   const BTM_RIGHT = [2, 5, 8];
-  let mar: CSS.Property.Margin<string | number> = "10px";
+  let mar: CSS.Property.Margin<string | number> = '10px';
   // const data = [
   //   [1, 7, 5, 4, 2, 6, 9, 8, 3],
   //   [6, 4, 2, 9, 3, 8, 5, 1, 7],
@@ -25,39 +25,55 @@ const Sudoku: React.FC<Props> = ({ index, data }) => {
   switch (modIndex) {
     case 1:
     case 2:
-      mar = "3px 10px 10px 10px";
+      mar = '3px 10px 10px 10px';
       break;
     case 5:
     case 0:
-      mar = "10px 10px 8px 10px";
+      mar = '10px 10px 8px 10px';
       break;
     default:
       break;
   }
 
   return (
-    <div className="flex">
+    <div className='flex'>
       {index}
       <div
-        className="sudoku-container"
+        className='sudoku-container'
         style={{
           margin: mar,
         }}
       >
         {data.map((row, r) => {
           return row.map((column, c) => {
-            let top: CSS.Property.BorderTopWidth = TOP_LEFT.includes(r) ? "3px" : "1px";
-            let right: CSS.Property.BorderRightWidth = BTM_RIGHT.includes(c) ? "3px" : "1px";
-            let btm: CSS.Property.BorderBottomWidth = BTM_RIGHT.includes(r) ? "3px" : "1px";
-            let left: CSS.Property.BorderLeftWidth = TOP_LEFT.includes(c) ? "3px" : "1px";
+            let top: CSS.Property.BorderTopWidth = TOP_LEFT.includes(r)
+              ? '3px'
+              : '1px';
+            let right: CSS.Property.BorderRightWidth = BTM_RIGHT.includes(c)
+              ? '3px'
+              : '1px';
+            let btm: CSS.Property.BorderBottomWidth = BTM_RIGHT.includes(r)
+              ? '3px'
+              : '1px';
+            let left: CSS.Property.BorderLeftWidth = TOP_LEFT.includes(c)
+              ? '3px'
+              : '1px';
             return (
               <div
-                className="sudoku-unit"
+                className='sudoku-unit'
                 style={{
                   borderTopWidth: top,
                   borderRightWidth: right,
                   borderBottomWidth: btm,
                   borderLeftWidth: left,
+                  borderLeftColor: left == '3px' ? 'black' : 'gray',
+                  borderRightColor: right == '3px' ? 'black' : 'gray',
+
+                  borderTopColor: top == '3px' ? 'black' : 'gray',
+
+                  borderBottomColor: btm == '3px' ? 'black' : 'gray',
+
+                  fontSize: 20,
                 }}
               >
                 {column}
